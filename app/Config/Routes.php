ed153c2d12e56a225e66ff8fa15182d3242da124
JESUS ARTURO CISNEROS CANTERO <JCISNEROS@PROFEDET20.GOB.MX>
2024-03-11 17:10:05 -0600

    codigo de ejemplo

---
+++ app/Config/Routes.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Controllers\Producto;
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+$routes->get('/producto', 'Producto::index');
+$routes->get('/producto/crear', 'Producto::mostrar');
+$routes->post('/producto/traer', 'Producto::traer');
+$routes->get('/producto/editar/(:num)/', 'Producto::editar/$1');
+$routes->post('/producto/actualizar', 'Producto::actualizar');
+$routes->get('/producto/borrar/(:num)', 'Producto::eliminar/$1');
+
+
+
+
+
+
+//$routes->post('/Producto/AgregarActualizar','Producto::AgregarActualizar');
